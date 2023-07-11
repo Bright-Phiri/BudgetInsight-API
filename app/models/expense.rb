f7fec81b37b82aa5c552fb8ce81b8e61dc2f78f3
Bright Phiri <bphiri.aki@gmail.com>
2023-07-11 11:11:28 +0200

    Add robocop

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,4 +4,5 @@ class Expense < ApplicationRecord
   belongs_to :category
   belongs_to :project, counter_cache: true
   validates :title, :description, presence: true
+  validates :amount, numericality: true
 end
