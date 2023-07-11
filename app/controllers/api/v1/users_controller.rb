221761d050f97a5c5ddccf2e97155b7f6fbfc0e3
Bright Phiri <bphiri.aki@gmail.com>
2023-07-11 19:30:47 +0200

    feat: Implement users controller actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,40 @@
 # frozen_string_literal: true
 
-class API::V1::UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:update, :show]
+  def index
+    users = User.all
+    render json: users
+  end
 
+  def show
+    render json: @user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
