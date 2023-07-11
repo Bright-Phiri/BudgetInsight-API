1159effe162d950167c009da46ad558b1b4e4a22
Bright Phiri <bphiri.aki@gmail.com>
2023-07-11 21:40:30 +0200

    feat: Update model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  VALID_CATEGORIES = ["Labor", "Materials", "Utilities", "Furniture"].freeze
+  VALID_CATEGORIES = ['Labor', 'Materials', 'Utilities', 'Furniture'].freeze
   validates :name, presence: true, inclusion: { in: VALID_CATEGORIES }
 end
