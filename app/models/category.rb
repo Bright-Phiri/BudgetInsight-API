49bc902df8008d4474838c5eeb2d25db2cca2b77
Bright Phiri <bphiri.aki@gmail.com>
2023-07-12 20:43:46 +0200

    feat: Update model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@
 class Category < ApplicationRecord
   VALID_CATEGORIES = ['Labor', 'Materials', 'Utilities', 'Furniture'].freeze
   validates :name, presence: true, inclusion: { in: VALID_CATEGORIES }
+  has_many :expenses
 end
